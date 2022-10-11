fdbdb85fd456cdce2810a67d91aae6a1214f8366
Julien <sofa.kreutz@yahoo.fr>
2022-10-11 23:11:34 +0200

    Few changes before final check

--- vagrant_project/install_ansible.sh
+++ vagrant_project/install_ansible.sh
@@ -11,7 +11,6 @@ ansible --version
 #-> Installation of python39 (optionnal)
 sudo -i sh <<EOF
 dnf install python39 -y
-pip3 install toml
 EOF
 
 #-> Installation of Git for the repo download
@@ -19,7 +18,18 @@ sudo -i sh <<EOF
 dnf install git -y
 EOF
 
-#-> Setup the git repository
-su - vagrant sh <<EOF
-git clone -b vagrant --single-branch https://github.com/sofackj/its_project_a.git
-EOF
+# #-> Setup the git repository
+# sudo -i <<EOF
+# cd /home/vagrant/
+# git clone -b vagrant --single-branch https://github.com/sofackj/its_project_a.git
+# chown -R vagrant:vagrant its_project_a
+# cd its_project_a/
+# ansible-inventory --graph
+# ansible-playbook main-playbook.yml --skip-tags update
+# EOF
+sudo -H -u vagrant bash -c 'git clone -b vagrant --single-branch \
+https://github.com/sofackj/its_project_a.git \
+/home/vagrant/its_project_a'
+sudo -H -u vagrant bash -c 'cd ~/its_project_a/;\
+ansible-inventory --graph;\
+ansible-playbook main-playbook.yml --skip-tags update'
