7056a68a6e152bbe1beae817ebaea8dea5aee7d6
Julien <sofa.kreutz@yahoo.fr>
2022-10-12 01:57:38 +0200

    Update combo vagrant-ansible

--- vagrant_project/install_ansible.sh
+++ vagrant_project/install_ansible.sh
@@ -19,9 +19,12 @@ dnf install git -y
 EOF
 
 #-> Setup the git repository
-sudo -H -u vagrant bash -c 'git clone -b vagrant --single-branch \
-https://github.com/sofackj/its_project_a.git \
-/home/vagrant/its_project_a'
-sudo -H -u vagrant bash -c 'cd ~/its_project_a/;\
+# sudo -H -u vagrant bash -c 'git clone -b vagrant --single-branch \
+# https://github.com/sofackj/its_project_a.git \
+# /home/vagrant/its_project_a'
+sudo -H -u vagrant bash -c 'cd ~/ansible_project/;\
 ansible-inventory --graph;\
 ansible-playbook main-playbook.yml --skip-tags update'
+# sudo -H -u vagrant bash -c 'cd ~/its_project_a/;\
+# ansible-inventory --graph;\
+# ansible-playbook main-playbook.yml --skip-tags update'
