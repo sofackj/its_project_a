70f660010f43fe0504969905cf69352643354f51
Julien <sofa.kreutz@yahoo.fr>
2022-10-11 14:50:14 +0200

    Vagrant major fixes

---
+++ vagrant_project/install-ansible.sh
@@ -0,0 +1,23 @@
+# Ansible controller installation
+
+#-> Installation of Ansible
+sudo -i sh <<EOF
+dnf update -y
+dnf install epel-release -y
+dnf install ansible -y
+EOF
+ansible --version
+
+#-> Installation of python39 (optionnal)
+sudo -i sh <<EOF
+dnf install python39 -y
+pip3 install toml
+EOF
+
+#-> Installation of Git for the repo download
+sudo -i sh <<EOF
+dnf install git -y
+EOF
+
+#-> Setup the git repository
+git clone -b vagrant --single-branch https://github.com/sofackj/its_project_a.git
