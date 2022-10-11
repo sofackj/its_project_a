6b6b58a938c9d3539ba8226b5a92d1693dbc809e
Julien <sofa.kreutz@yahoo.fr>
2022-10-12 00:01:51 +0200

    Fix vault process with vagrant

--- vagrant_project/install_ansible.sh
+++ vagrant_project/install_ansible.sh
@@ -1,35 +1,27 @@
-# Ansible controller installation
+# # Ansible controller installation
 
-#-> Installation of Ansible
-sudo -i sh <<EOF
-dnf update -y
-dnf install epel-release -y
-dnf install ansible -y
-EOF
-ansible --version
+# #-> Installation of Ansible
+# sudo -i sh <<EOF
+# dnf update -y
+# dnf install epel-release -y
+# dnf install ansible -y
+# EOF
+# ansible --version
 
-#-> Installation of python39 (optionnal)
-sudo -i sh <<EOF
-dnf install python39 -y
-EOF
+# #-> Installation of python39 (optionnal)
+# sudo -i sh <<EOF
+# dnf install python39 -y
+# EOF
 
-#-> Installation of Git for the repo download
-sudo -i sh <<EOF
-dnf install git -y
-EOF
+# #-> Installation of Git for the repo download
+# sudo -i sh <<EOF
+# dnf install git -y
+# EOF
 
 # #-> Setup the git repository
-# sudo -i <<EOF
-# cd /home/vagrant/
-# git clone -b vagrant --single-branch https://github.com/sofackj/its_project_a.git
-# chown -R vagrant:vagrant its_project_a
-# cd its_project_a/
-# ansible-inventory --graph
-# ansible-playbook main-playbook.yml --skip-tags update
-# EOF
-sudo -H -u vagrant bash -c 'git clone -b vagrant --single-branch \
-https://github.com/sofackj/its_project_a.git \
-/home/vagrant/its_project_a'
-sudo -H -u vagrant bash -c 'cd ~/its_project_a/;\
-ansible-inventory --graph;\
-ansible-playbook main-playbook.yml --skip-tags update'
+# sudo -H -u vagrant bash -c 'git clone -b vagrant --single-branch \
+# https://github.com/sofackj/its_project_a.git \
+# /home/vagrant/its_project_a'
+# sudo -H -u vagrant bash -c 'cd ~/its_project_a/;\
+# ansible-inventory --graph;\
+# ansible-playbook main-playbook.yml --skip-tags update'
